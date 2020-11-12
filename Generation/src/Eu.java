942e3e1039bf40720560330270cc52f40ce51b7c
ThiagoBarassa <thiagobarassa@gmail.com>
2020-11-12 17:57:15 -0300

    commit com os primiros exercicios java

---
+++ Generation/src/Eu.java
@@ -0,0 +1,12 @@
+
+public class Eu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Meus maiores hobbies são: Jogar futebol "+
+		"sair com meus amigos, jogar video game e pescar...\n"+
+					"Agora situaçoes que não gosto é saber que tenho algo a fazer "+
+		"e ainda não fiz");
+	}
+
+}
---
+++ Generation/src/Mentalidades.java
@@ -0,0 +1,10 @@
+
+public class Mentalidades {
+
+	public static void main(String[] args) {
+		System.out.println("As mentalidades mais utilizadas foram "
+				+ "Persistencia e Orientação ao Futuro");
+
+	}
+
+}
